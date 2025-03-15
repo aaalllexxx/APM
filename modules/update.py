5f2d1703e5c53f31beb9b55c64a1da83cf993b37
aaalllexxx <a.aabdelnur@mail.ru>
2025-03-16 01:38:09 +0300

    fixed update

--- modules/update.py
+++ modules/update.py
@@ -35,3 +35,5 @@ def run(base_dir, *args, **kwargs):
     print("[green bold][+] Исходники установлены [/green bold]")
     print("[green bold][+] Запуск скрипта обновления [/green bold]")
     subprocess.run(["cmd.exe", "/c", "start", app_data + os.sep + "apm/sources/scripts/setup.bat"])
+    print("[green bold][+] Готово [/green bold]")
+
