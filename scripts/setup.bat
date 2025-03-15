48d786c2744081cea4eab46c4a15650046f5597f
aaalllexxx <a.aabdelnur@mail.ru>
2025-03-16 00:22:02 +0300

    fixed setup

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -7,7 +7,4 @@ echo %parent_folder%
 
 xcopy /E /I /Y "%parent_folder%" "%AppData%\apm"
 setx PATH "%AppData%\apm;%PATH%" /M
-if exist "%AppData%\apm\sources" (
-    rd /q /s "%AppData%\apm\sources"
-)
-exit
+cmd /c start %AppData%\apm\remove_sources.bat
