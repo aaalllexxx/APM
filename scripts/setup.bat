57c5af906172cf157fcd6d9a0e1eb757cb122463
aaalllexxx <a.aabdelnur@mail.ru>
2025-03-16 00:48:54 +0300

    fixed update

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -6,5 +6,11 @@ set "parent_folder=%cd%"
 echo %parent_folder%
 
 xcopy /E /I /Y "%parent_folder%" "%AppData%\apm"
-setx PATH "%AppData%\apm;%PATH%" /M
+
+rem Проверяем, есть ли путь уже в PATH
+echo %PATH% | find /i "%AppData%\apm" >nul 2>&1
+if %errorlevel% neq 0 (
+    setx PATH "%AppData%\apm;%PATH%" /M
+)
+
 exit
