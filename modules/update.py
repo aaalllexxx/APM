a72e96915b33915c560048bfa76d0b231d8f1f9b
aaalllexxx <a.aabdelnur@mail.ru>
2025-03-16 14:17:43 +0300

    edited message in update

--- modules/update.py
+++ modules/update.py
@@ -20,5 +20,5 @@ def run(base_dir, *args, **kwargs):
     print("[green bold][+] Исходники установлены [/green bold]")
     print("[green bold][+] Запуск скрипта обновления [/green bold]")
     subprocess.run(["cmd.exe", "/c", "start", app_data + os.sep + "apm/sources/scripts/setup.bat"])
-    print("[green bold][+] Готово [/green bold]")
+    print("[green bold][+] Готово. Дождитесь выполнения скрипта. [/green bold]")
 
