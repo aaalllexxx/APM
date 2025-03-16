6c882d1c74b0cbac3b1de508252715f528d0854a
aaalllexxx <a.aabdelnur@mail.ru>
2025-03-16 21:22:59 +0300

    edited remove logic to work with global modules

--- modules/remove.py
+++ modules/remove.py
@@ -3,9 +3,14 @@ import os
 from helpers import clear_dir
 from rich import print
 
-def run(*args, **kwargs):
-    arg = kwargs["args"]
-    module = arg[1]
+def run(base_dir, *args, **kwargs):
+    arg:list = kwargs["args"]
+    module = arg[-1]
+    path = ".apm/installed"
+    if "-g" in arg:
+        arg.pop(arg.index("-g"))
+        path = base_dir + "installed"
+
     if not os.path.exists(".apm"):
         print("[red][-] Директория не является проектом apm[/red]")
         return
@@ -13,5 +18,5 @@ def run(*args, **kwargs):
         print("[red][-] Модуль не найден[/red]")
         return
     
-    clear_dir(f".apm/installed/{module}")
+    clear_dir(f"{path}/{module}")
     print("[green][+] Модуль удален[/green]")
