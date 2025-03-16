91597efe18b5c32a00f01ddf7f41016aa13ec2e7
aaalllexxx <a.aabdelnur@mail.ru>
2025-03-16 13:32:37 +0300

    edited install

--- modules/install.py
+++ modules/install.py
@@ -1,7 +1,8 @@
 __help__ = "Установка программных модулей"
-from git import Repo
+from git import Repo, exc
 from helpers import clear_dir
 import os
+from rich import print
 
 def run(base_dir, *args, **kwargs):
     arg:list = kwargs["args"]
@@ -9,6 +10,11 @@ def run(base_dir, *args, **kwargs):
     name = url.split("/")[-1].replace(".git", "")
     if not os.path.exists("installed"):
         os.mkdir("installed")
-    Repo.clone_from(url, f"installed/{name}")
+    try:
+        Repo.clone_from(url, f"installed/{name}")
+    except exc.GitError:
+        print(f"[red][-] {url} не является репозиторием git[/red]")
+        return
     clear_dir(f"installed/{name}/.git")
+    print("[green][+] Модуль установлен[/green]")
     
